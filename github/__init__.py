723fa3c77b745f698841a50e6c5e6b1b6bbab71c
Malled <malte@drabesch.de>
2015-10-23 01:58:24 +0200

    added github repo event

--- github/__init__.py
+++ github/__init__.py
@@ -26,22 +26,40 @@ def shutdown(bot):
 def webhook():
     event = request.headers.get('X-GitHub-Event')
     try:
+        data = json.loads(request.data)
         if event == 'push':
-            handle_push_event()
+            handle_push_event(data)
+        elif event == 'repository':
+            handle_repository_event(data)
     except Exception as e:
+        print  e.message
         pass
     return "OK"
 
+def handle_repository_event(data):
+    url = github_shortify(data['repository']['html_url'])
+    bot_say("{} {} {}  repository: {}".format(data['sender']['login'],
+                                              data['action'],
+                                              data['repository']['full_name'],
+                                              url))
 
-def handle_push_event():
-    global local_bot
-    url = ''
-    data = json.loads(request.data)
-    r = requests.post("http://git.io", data={'url':data['compare'] })
+
+def handle_push_event(data):
+    url = github_shortify(data['compare'])
+    bot_say("[{}] {} pushed {} new commit{}: {}".format(data['repository']['name'],
+                                              data['pusher']['name'],
+                                              len(data['commits']),
+                                              "s" if (len(data['commits']) > 1) else "",
+                                              url))
+
+
+def github_shortify(url):
+    r = requests.post("http://git.io", data={'url':url })
     if r.status_code == 201:
-        url = r.headers['location']
+        return r.headers['location']
+    return ""
+
+def bot_say(msg):
+    global local_bot
     for c in local_bot.config.core.channels:
-        local_bot.msg(c,"[{}] {} pushed {} new commit: {}".format(data['repository']['name'],
-                                                                  data['pusher']['name'],
-                                                                  len(data['commits']),
-                                                                  url))
+        local_bot.msg(c,msg)
