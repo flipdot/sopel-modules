1ddca35bd9303041909404e97d927602a3e09fca
Felix Möller <git@felix-moeller.net>
2020-04-01 06:49:12 +0200

    Adapt to new format

--- covid/__init__.py
+++ covid/__init__.py
@@ -4,6 +4,8 @@ import locale
 import re
 import requests
 
+import pandas as pd
+
 from bs4 import BeautifulSoup
 from datetime import datetime
 from sopel import module
@@ -31,14 +33,25 @@ TS_FORMATS = [
     'Stand: %d. %B %Y; %H.%M Uhr',
 ]
 
+LOC_LUT = {
+    'Stadt Kassel': 'Kassel Stadt',
+    'Landkreis Kassel': 'Kassel Land',
+}
+
+REPR_LUT = {
+    'Gemeldete Fälle insgesamt': 'insgesamt',
+    'Genesene': 'genesen',
+    'Aktuell Infizierte': 'infiziert',
+    'Todesfälle': 'tot',
+}
+
 
 class CovidSection(StaticSection):
     announce_channel = ValidatedAttribute('announce_channel', default='#flipdot-covid')
     url = ValidatedAttribute('url', default='https://kassel.de/coronavirus')
-    cases_default = ValidatedAttribute('cases_default', default={"SK Kassel": {"Aktualisierung": 0, "Fallzahlen": 0, "Death": 0}, "LK Kassel": {"Aktualisierung": 0, "Fallzahlen": 0, "Death": 0}})
 
 
-def update_check(cases, update_raw):
+def update_check(ts_old, update_raw):
     locale.setlocale(locale.LC_ALL, 'de_DE.UTF-8')
     soup = BeautifulSoup(update_raw, 'html.parser')
     ts_str = soup.find('h1', 'SP-Headline--paragraph').text
@@ -51,76 +64,49 @@ def update_check(cases, update_raw):
             pass
     if ts_new is None:
         raise SyntaxError(f"No time format could parse the raw string '{ts_str}'.")
-    k = [e.text for e in soup.thead.find_all('strong')]
-    l = len(k)
-    v_raw = soup.table.find_all('td')[l:]
-    s = len(v_raw) // l
-    v_pre = {v_raw[i * l].text: v_raw[i * l + 1:(i + 1) * l] for i in range(s)}
-    ts_old = cases[list(cases.keys())[0]].get(KEY_TIME)
-    if int(ts_old) < int(ts_new):
-        update_required = True
     else:
         update_required = False
-    return update_required, {
-        'SK Kassel': {
-            'Aktualisierung': ts_new,
-            'Fallzahlen': v_pre['Stadt Kassel'][0].text,
-            'Fallzahlen_rel': v_pre['Stadt Kassel'][1].text.split('(')[0].replace(' ', ''),
-            'Death': v_pre['Stadt Kassel'][2] if len(v_pre['Stadt Kassel']) > 2 else 0,
-            # 'Death_rel': FIXME
-        },
-        'LK Kassel': {
-            'Aktualisierung': ts_new,
-            'Fallzahlen': v_pre['Landkreis Kassel'][0].text,
-            'Fallzahlen_rel': v_pre['Landkreis Kassel'][1].text.split('(')[0].replace(' ', ''),
-            'Death': v_pre['Landkreis Kassel'][2] if len(v_pre['Landkreis Kassel']) > 2 else 0,
-            # 'Death_rel': FIXME
-        },
-    }
+    if int(ts_old) < int(ts_new):
+        update_required = True
+    data_cases = pd.read_html(str(soup.table))[0].set_index('Vorkommen')
+    return update_required, ts_new, data_cases
 
 
 def update_repr(prefix, update_data_pre, add_prefix=False):
     first_line = True
     ret = ''
     if add_prefix:
-        ret += f'{prefix} '
-    for location, data in update_data_pre.items():
+        ret += f"{prefix} "
+    for loc in LOC_LUT.keys():
         if not first_line:
-            ret += '\n'.ljust(len(prefix) + 3)
-        m = re.search(r'^(SK|LK)\s([A-Za-z ]+)$', location)
-        if m is not None:
-            district, city = m.groups()
-        else:
-            district, city = None, location
+            first_ljust_len = len(LOC_LUT[list(LOC_LUT.keys())[0]]) - len(prefix)
+            ret += '\n'.ljust(first_ljust_len)
         first_line = False
-        ret += f'{city} '
-        if district is not None:
-            if district == 'SK':
-                ret += 'Stadt'
-            elif district == 'LK':
-                ret += 'Land'
-            ret += ': '
-        ret += f"{data['Fallzahlen']} "
-        cases_rel = data.get('Fallzahlen_rel')
-        if cases_rel:
-            ret += f"({cases_rel}) "
-        ret += "infiziert"
-        death = data['Death']
-        if death > 0:
-            ret += f", {data['Death']} tot"
-        # death_rel = data['Death_rel'] # FIXME
+        ret += f"{LOC_LUT[loc]}: "
+        first_field = True
+        for k, v in REPR_LUT.items():
+            field = update_data_pre.loc[loc][k]
+            try:
+                if int(field) == 0:
+                    continue
+            except:
+                pass
+            if not first_field:
+                ret += ', '
+            first_field = False
+            ret += f"{field} {v}"
     return ret
 
 
 @module.interval(5 * 60)
 def covid_update(bot, dest=None, update_forced=False):
     print(f"{PREFIX} Checking COVID-19 data...")
-    cases = cache_load(bot)
+    ts_old, cases = cache_load(bot)
     req = requests.get(bot.config.covid.url)
     if req.status_code != 200:
         raise ConnectionError("Could not download covid data.")
     update_raw = req.text
-    update_required, update_data = update_check(cases, update_raw)
+    update_required, ts_old, update_data = update_check(ts_old, update_raw)
     if not update_required and not update_forced:
         print(f"{PREFIX} No update...")
         return
@@ -129,7 +115,7 @@ def covid_update(bot, dest=None, update_forced=False):
         dest = bot.config.covid.announce_channel
     for line in f"{COLOR_PREFIX} {msg}".split('\n'):
         bot.say(line, dest)
-    cache_save(bot, update_data)
+    cache_save(bot, ts_old, update_data)
     print(f"{PREFIX} Updates cached...")
 
 
@@ -137,7 +123,8 @@ def covid_update(bot, dest=None, update_forced=False):
 def covid_clear(bot, trigger):
     print(f"{PREFIX} Clearing COVID-19 cache...")
     chan = bot.config.covid.announce_channel
-    bot.db.set_channel_value(chan, 'cases', bot.config.covid.cases_default)
+    bot.db.set_channel_value(chan, 'ts_old', 0)
+    bot.db.set_channel_value(chan, 'cases', None)
 
 
 @module.commands('covid')
@@ -151,14 +138,20 @@ def setup(bot):
 
 def cache_load(bot):
     chan = bot.config.covid.announce_channel
-    cases_str = bot.db.get_channel_value(chan, 'cases') or bot.config.covid.cases_default
+    ts_old = bot.db.get_channel_value(chan, 'ts_old') or 0
+    cases_bot = bot.db.get_channel_value(chan, 'cases')
+    if isinstance(cases_bot, pd.DataFrame):
+        cases_str = pd.read_json(cases_bot)
+    else:
+        cases_str = None
     if isinstance(cases_str, str):
         cases = json.loads(cases_str)
     else:
         cases = cases_str
-    return cases
+    return ts_old, cases
 
 
-def cache_save(bot, update_data):
+def cache_save(bot, ts_old, update_data):
     chan = bot.config.covid.announce_channel
-    bot.db.set_channel_value(chan, 'cases', update_data)
+    bot.db.set_channel_value(chan, 'ts_old', ts_old)
+    bot.db.set_channel_value(chan, 'cases', update_data.to_json())
